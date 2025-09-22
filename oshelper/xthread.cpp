a25c99071e3902d5a32ea2f53f18c906ff4db0a6
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-09-22 20:05:43 +0800

    改写xthread

--- oshelper/xthread.cpp
+++ oshelper/xthread.cpp
@@ -1,1 +1,102 @@
 #include <xthread.hpp>
+#include <criticalarea.hpp>
+#if defined(FREERTOS) || defined(USE_FREERTOS)
+#include <task.h>
+
+void XThreadBase::taskReturn() noexcept
+{ vTaskDelete(nullptr); }
+
+void XThreadBase::sleep_for(std::size_t const ms) noexcept
+{ vTaskDelay(ms); }
+
+void XThreadBase::sleep_until(std::size_t & pre,std::size_t const ms) noexcept
+{ vTaskDelayUntil(reinterpret_cast<TickType_t*>(&pre),ms); }
+
+std::size_t XThreadBase::thread_count() noexcept
+{ return m_th_cnt_.load(std::memory_order_relaxed); }
+
+void * XThreadBase::thread_handle() const noexcept
+{ return m_thread_.load(std::memory_order_acquire); }
+
+int XThreadBase::thread_id() const noexcept
+{ return m_id_.load(std::memory_order_relaxed); }
+
+void XThreadBase::start() const noexcept {
+    if (m_thread_.load(std::memory_order_relaxed)) {
+        vTaskResume(static_cast<TaskHandle_t>(m_thread_.load(std::memory_order_relaxed)));
+    }
+}
+
+void XThreadBase::stop() const noexcept {
+    if (m_thread_.load(std::memory_order_relaxed)) {
+        vTaskSuspend(static_cast<TaskHandle_t>(m_thread_.load(std::memory_order_relaxed)));
+    }
+}
+
+void XThreadBase::setPriority(uint32_t const p) const noexcept {
+    if (m_thread_.load(std::memory_order_relaxed)) {
+        vTaskPrioritySet(static_cast<TaskHandle_t>(m_thread_.load(std::memory_order_relaxed)),p);
+    }
+}
+
+void XThreadBase::destroy() noexcept {
+    if (m_thread_.load(std::memory_order_relaxed)) {
+        vTaskSuspend(static_cast<TaskHandle_t>(m_thread_.load(std::memory_order_relaxed)));
+        vTaskDelete(static_cast<TaskHandle_t>(m_thread_.load(std::memory_order_relaxed)));
+        m_thread_.store({},std::memory_order_relaxed);
+    }
+}
+
+XThreadBase::~XThreadBase()
+{ destroy(); }
+
+void XThreadBase::setInfo(void * const th) {
+    if (th) {
+        auto const th_ { static_cast<tskTaskControlBlock*>(th) };
+        m_thread_.store(th_,std::memory_order_relaxed);
+        m_id_ = static_cast<int>(m_th_cnt_.fetch_add(1,std::memory_order_relaxed));
+        vTaskSuspend(th_);
+    }
+}
+
+void XThreadBase::swap(XThreadBase & o) noexcept {
+    m_id_.store(o.m_id_.exchange(m_id_.load(std::memory_order_relaxed),std::memory_order_relaxed),std::memory_order_relaxed);
+    m_thread_.store(o.m_thread_.exchange(m_thread_.load(std::memory_order_relaxed),std::memory_order_relaxed),std::memory_order_relaxed);
+}
+
+void XThreadBase::createTask(void(*f)(void*)
+                                , std::size_t const uxStackDepth
+                                , void * const pvParameters
+                                , void * const puxStackBuffer
+                                , void * const pxTaskBuffer) noexcept
+{
+    XStringStream ss {};
+    ss << GET_STR(thread:) << m_th_cnt_.load(std::memory_order_relaxed);
+    TaskCriticalArea c{};
+#if configSUPPORT_STATIC_ALLOCATION > 0
+    if (puxStackBuffer && pxTaskBuffer) {
+        auto const th{ xTaskCreateStatic(f,ss.str().c_str(),uxStackDepth,pvParameters,configMAX_PRIORITIES
+            ,static_cast<StackType_t*>(puxStackBuffer),static_cast<StaticTask_t*>(pxTaskBuffer)) };
+        setInfo(th);
+        return;
+    }
+#endif
+
+#if configSUPPORT_DYNAMIC_ALLOCATION > 0
+    tskTaskControlBlock * th{};
+    xTaskCreate(f,ss.str().c_str(),uxStackDepth,pvParameters,configMAX_PRIORITIES,&th);
+    setInfo(th);
+#endif
+}
+
+#if configSUPPORT_DYNAMIC_ALLOCATION > 0
+
+XThreadDynamic:: XThreadDynamic(XThreadDynamic && o) noexcept
+{ XThreadBase::swap(o);}
+
+XThreadDynamic & XThreadDynamic::operator=(XThreadDynamic && o) noexcept
+{ XThreadDynamic{std::move(o)}.swap(*this); return *this; }
+
+#endif
+
+#endif
--- oshelper/xthread.hpp
+++ oshelper/xthread.hpp
@@ -1,164 +1,62 @@
-#ifndef XTHREAD_HPP
-#define XTHREAD_HPP
+#ifndef  X_THREAD_HPP
+#define X_THREAD_HPP
 
-#include <FreeRTOS.h>
-#include <task.h>
-#include <xhelper.hpp>
-#include <sstream>
-
+#include <xthreadbase.hpp>
 #if defined(FREERTOS) || defined(USE_FREERTOS)
+#include <FreeRTOS.h>
 
-class Task_CRITICAL final {
-    X_DISABLE_COPY_MOVE(Task_CRITICAL)
-    std::atomic_ulong m_cnt_{};
+#if configSUPPORT_DYNAMIC_ALLOCATION > 0
 
+class XThreadDynamic final : public XThreadBase {
+    X_DISABLE_COPY(XThreadDynamic)
 public:
-    explicit Task_CRITICAL() noexcept
-    { enter(); }
+    XThreadDynamic() = default;
 
-    ~Task_CRITICAL() noexcept
-    { exit(); }
+    ~XThreadDynamic() override = default;
 
-    void enter() noexcept
-    { ++m_cnt_; taskENTER_CRITICAL(); }
+    template<typename ...Args_>
+    explicit XThreadDynamic(std::size_t const stack_depth,Args_ && ...args) noexcept
+    { start_(stack_depth,{},{},std::forward<Args_>(args)...); }
 
-    void exit() noexcept
-    { --m_cnt_; taskEXIT_CRITICAL(); }
+    XThreadDynamic(XThreadDynamic && ) noexcept;
+
+    XThreadDynamic & operator=(XThreadDynamic && ) noexcept;
 };
 
-template<uint32_t > class XThread_;
-
-template<uint32_t STACK_DEPTH = {}>
-class XThread_ final {
-
-    X_DISABLE_COPY(XThread_)
-
-    struct [[maybe_unused]] Static_Task_Data {
-        std::array<uint32_t, STACK_DEPTH> m_stack{};
-        StaticTask_t m_tcb{};
-    };
-
-    void destroy() noexcept {
-        if (m_thread_) {
-            vTaskSuspend(m_thread_);
-            vTaskDelete(m_thread_);
-            m_thread_ = {};
-        }
-    }
-
-    template <typename Tuple_, size_t... Indices_>
-    static void Invoke_(void* RawVals_) noexcept {
-        const std::unique_ptr<Tuple_> FnVals_(static_cast<Tuple_*>(RawVals_));
-        Tuple_& Tup_ {*FnVals_.get()};
-        std::invoke(std::move(std::get<Indices_>(Tup_))...);
-        vTaskDelete(nullptr);
-    }
-
-    template <typename Tuple_, size_t... Indices_>
-    [[nodiscard]] static constexpr auto Get_invoke_(std::index_sequence<Indices_...>) noexcept {
-        return &Invoke_<Tuple_, Indices_...>;
-    }
-
-    template<typename Fn_,typename ...Args_>
-    void Start_(uint32_t stack_depth,Fn_ &&fn,Args_ &&...args) noexcept {
-        static std::atomic_int thread_index{};
-        std::stringstream ss;
-        ss << GET_STR(thread:) << thread_index;
-
-        using Tuple_ = std::tuple<std::decay_t<Fn_>, std::decay_t<Args_>...>;
-        auto Decay_copied_{std::make_unique<Tuple_>(std::forward<Fn_>(fn),std::forward<Args_>(args)...)};
-        constexpr auto Invoker_proc { Get_invoke_<Tuple_>(std::make_index_sequence<1 + sizeof...(Args_)>{}) };
-
-        tskTaskControlBlock* th_{};
-        Task_CRITICAL c{}; /*进入临界区*/
-        if (STACK_DEPTH > 0) {
-#if configSUPPORT_STATIC_ALLOCATION > 0 && !(configSUPPORT_DYNAMIC_ALLOCATION > 0)
-            stack_depth = STACK_DEPTH;
-            StaticTask_t tcb_{};
-            th_ = xTaskCreateStatic(Invoker_proc,ss.str().c_str(),
-                stack_depth,Decay_copied_.get(),
-                configMAX_PRIORITIES,m_std_.load().m_stack,&tcb_);
-            m_std_.load().m_tcb = tcb_;
 #endif
-        }else {
-#if configSUPPORT_DYNAMIC_ALLOCATION > 0
-            (void)xTaskCreate(Invoker_proc,ss.str().c_str(),
-                stack_depth,Decay_copied_.get(),configMAX_PRIORITIES,&th_);
-#endif
-        }
 
-        if (th_) {
-            m_thread_ = th_;
-            m_id = thread_index.load();
-            m_th_cnt_ = ++thread_index;
-            Decay_copied_.release();
-            vTaskSuspend(th_);
-        }
-    }
+#if configSUPPORT_STATIC_ALLOCATION > 0
+
+template<std::size_t DEPTH>
+class XThreadStatic final : public XThreadBase {
+    X_DISABLE_COPY(XThreadStatic)
+    static_assert(DEPTH > 0,"DEPTH must be greater than 0");
+    std::array<std::size_t, DEPTH> m_stack_{};
+    StaticTask_t m_tcb_{};
 
 public:
-    [[nodiscard]] auto thread_handle() const noexcept
-    { return m_thread_.load(); }
+    XThreadStatic() = default;
 
-    static auto thread_count() noexcept
-    { return m_th_cnt_.load(); }
+    ~XThreadStatic() override = default;
 
-    [[nodiscard]] auto thread_id() const noexcept
-    { return m_id.load(); }
+    template<typename ...Args_>
+    explicit XThreadStatic(Args_ && ...args) noexcept
+    { start_(DEPTH,m_stack_.data(),std::addressof(m_tcb_),std::forward<Args_>(args)...); }
 
-    void Start() const noexcept
-    { if (m_thread_) { vTaskResume(m_thread_); } }
+    XThreadStatic(XThreadStatic && o) noexcept
+    { swap(o); }
 
-    void Stop() const noexcept
-    { if (m_thread_) { vTaskSuspend(m_thread_); } }
-
-    void Set_Priority(const uint32_t &p) const noexcept
-    { if (m_thread_){ vTaskPrioritySet(m_thread_,p); } }
-
-    template<typename Fn_,typename ...Args_>
-    explicit XThread_(const uint32_t &stack_depth,Fn_ &&fn,Args_&& ...args)
-    { Start_(stack_depth,std::forward<Fn_>(fn),std::forward<Args_>(args)...); }
-
-    XThread_(XThread_ &&obj) noexcept {
-        m_thread_ = obj.m_thread_.exchange({});
-#if configSUPPORT_STATIC_ALLOCATION > 0
-        if(STACK_DEPTH > 0) {
-            m_std_ = obj.m_std_.exchange({});
-        }
-#endif
-    }
-
-    XThread_& operator=(XThread_ &&obj) noexcept {
-        if (this != &obj) {
-            destroy();
-            m_thread_ = obj.m_thread_.exchange({});
-#if configSUPPORT_STATIC_ALLOCATION > 0
-            if(STACK_DEPTH > 0) {
-                m_std_ = obj.m_std_.exchange({});
-            }
-#endif
-        }
-        return *this;
-    }
-
-    ~XThread_()
-    { destroy(); }
-
-    XThread_() = default;
+    XThreadStatic & operator=(XThreadStatic && o) noexcept
+    { XThreadStatic{std::move(o)}.swap(*this); return *this; }
 
 private:
-    inline static std::atomic_uint32_t m_th_cnt_{};
-    std::atomic_int m_id{-1};
-    std::atomic<tskTaskControlBlock *> m_thread_{};
-#if configSUPPORT_STATIC_ALLOCATION > 0
-    std::atomic<Static_Task_Data> m_std_{};
-#endif
+    void swap(XThreadBase & o) noexcept override {
+        XThreadBase::swap(o);
+        m_stack_.swap(static_cast<XThreadStatic &>(o).m_stack_);
+        std::swap(m_tcb_,static_cast<XThreadStatic &>(o).m_tcb_);
+    }
 };
 
-using XThread_Dynamic = XThread_<>;
-
-template<uint32_t STACK_DEPTH = 1024>
-using XThread_Static = XThread_<STACK_DEPTH>;
-
+#endif
 #endif
 #endif
