c8b8dfbe7b9c56667d4bd0d5cc728f74dabf577b
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-09-25 18:48:29 +0800

    2025-09-25 update

--- char_dev/include/chardevbase.hpp
+++ char_dev/include/chardevbase.hpp
@@ -9,20 +9,20 @@ protected:
 public:
     virtual ~CharDevBase() = default;
 
-    virtual int64_t read(uint8_t *, std::size_t, int64_t) = 0;
+    virtual int64_t read(void *, std::size_t, int64_t) = 0;
 
-    virtual int64_t read(uint8_t *, std::size_t, int64_t) const = 0;
+    virtual int64_t read(void *, std::size_t, int64_t) const = 0;
 
-    virtual int64_t write(const uint8_t *, std::size_t, int64_t) = 0;
+    virtual int64_t write(const void *, std::size_t, int64_t) = 0;
 
-    virtual int64_t write(const uint8_t *, std::size_t, int64_t) const = 0;
+    virtual int64_t write(const void *, std::size_t, int64_t) const = 0;
 
     virtual void startDMAReceive() {}
 
 protected:
-    virtual int64_t isrIn(uint8_t *, std::size_t) { return 0; }
+    virtual int64_t isrIn(void *, std::size_t) { return 0; }
 
-    virtual int64_t isrIn(uint8_t *, std::size_t) const { return 0; }
+    virtual int64_t isrIn(void *, std::size_t) const { return 0; }
 
     virtual int64_t isrDMAIn(std::size_t) { return 0; }
 
