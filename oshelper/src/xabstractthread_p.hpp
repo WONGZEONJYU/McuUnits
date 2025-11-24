a548772edcef5619fc146d0b6f71c79cdb061a95
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-11-24 15:02:28 +0800

    2025-11-24 update

--- oshelper/src/xabstractthread_p.hpp
+++ oshelper/src/xabstractthread_p.hpp
@@ -2,27 +2,31 @@
 #define X_ABSTRACT_THREAD_P_HPP 1
 
 #include <xabstractthread.hpp>
-#include <semaphore.hpp>
+#include <conditionvariable.hpp>
+#include <mutex.hpp>
 
 #if 1
-class XAbstractThreadPrivate final : public XAbstractThreadData {
+class XAbstractThreadPrivate : public XAbstractThreadData {
 
 public:
     W_DECLARE_PUBLIC(XAbstractThread)
 
     using CallablePtr = XCallableHelper::CallablePtr;
 
-    inline static XAtomicInteger<std::size_t> m_th_cnt_{};
-    XAtomicInt m_id_{-1};
-    XAtomicPointer<void> m_thread_{};
-    BinarySemaphore m_sem{};
+    inline static XAtomicInteger<std::size_t> m_th_cnt{};
+
+    XAtomicInt m_id{-1};
+    XAtomicPointer<void> m_thread{};
+    Mutex m_mtx{};
+    ConditionVariableAny m_cv{};
     XAtomicBool m_finished{true},m_detached{};
     CallablePtr m_fn{};
 
     static void start(void *);
     explicit XAbstractThreadPrivate() = default;
     ~XAbstractThreadPrivate() override = default;
-    void startHelper() noexcept;
+    void startHelper(std::size_t) noexcept;
+    void finished() noexcept;
 };
 
 #endif
